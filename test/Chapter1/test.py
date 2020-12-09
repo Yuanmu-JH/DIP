0151d36a22678e22f201dbfb1a12de9de0ca8d6b
Yuanmu-JH <hahayuanmu@163.com>
2020-12-09 14:54:18 +0800

    整理

--- test/Chapter1/test.py
+++ test/Chapter1/test.py
@@ -1,7 +1,7 @@
 from skimage import io
 from matplotlib import pyplot as plt    #导入绘图模块
 
-file_name='test.jpg'
+file_name= 'Charapter1/test.jpg'
 image=io.imread(file_name)
 plt.imshow(image)   #进行图片绘制
 plt.show()          #显示绘制图像
